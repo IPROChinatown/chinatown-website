31f64b98327d3ef15e087b1d6e7b318ab786d80f
Keith Bateman <kbateman@hawk.iit.edu>
2018-11-27 19:35:38 -0600

    Added language changer and streamlined map code

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,69 +1,90 @@
 // To the extent possible under law, I waive all copyright and related
 // or neighboring rights to this program via CC0
 
-var pingtom = {name: "Ping Tom Park",
-			   position: {lat: 41.859115900, lng: -87.632596900},
-			   icon: "/images/map-icons/pinghut.png",
-			   link: "./Location/PingTomPark.html"};
+var locations = [
+	/*
+{"name": "Nine Dragon Wall",
+  "address": "158 W Cermak Rd",
+  "position": {"lat": 41.853170000, "lng": -87.631345000},
+  "icon": "/images/map-icons/NineDragonWallIcon.png",
+  "link": "./Location/NineDragonWall.html"},
+ {"name": "Chinatown Gate",
+  "address": "Wentworth and Cermak",
+  "position": {"lat": 41.852697400, "lng": -87.632211300},
+  "icon": "",
+  "link": ""},
+ {"name": "Pui Tak Center",
+  "address": "2216 S Wentworth Ave",
+  "position": {"lat": 41.852396900, "lng": -87.632291100},
+  "icon": "/images/map-icons/PuiTakCenterIcon.png",
+  "link": "./Location/PuiTakCenter.html"},
+*/
+ {"name": "International Buddhist Friendship Association",
+  "address": "2249 S Wentworth Ave",
+  "position": {"lat": 41.851436200, "lng": -87.631652300},
+  "icon": "/images/map-icons/dragon.png",
+  "link": "location"} //TODO: proper link to location
+	/*,
+ {"name": "Chinese Christian Union Church",
+  "address": "2301 S Wentworth Ave",
+  "position": {"lat": 41.850719000, "lng": -87.631671600},
+  "icon": "/images/map-icons/ChineseChristianUnionChurchIcon.png",
+  "link": "./Location/ChineseChristianCatholicChurch.html"},
+ {"name": "Herbalist",
+  "address": "2400 S Wentworth Ave",
+  "position": {"lat": 41.849052000, "lng": -87.632070700},
+  "icon": "",
+  "link": ""},
+ {"name": "Chinese Shop",
+  "address": "2316 S Wentworth Ave",
+  "position": {"lat": 41.850397200, "lng": -87.632185900},
+  "icon": "",
+  "link": ""},
+ {"name": "Chinese-American Museum",
+  "address": "238 W 23rd St",
+  "position": {"lat": 41.851217700, "lng": -87.633518200},
+  "icon": "/images/map-icons/dragon.png",
+  "link": "./Location/Chinese-AmericanMuseum.html"},
+ {"name": "St Therese Church",
+  "address": "218 W Alexander St",
+  "position": {"lat": 41.851545200, "lng": -87.632607200},
+  "icon": "/images/map-icons/StThereseChurchIcon.png",
+  "link": "./Location/ChinatownChurch-SaintTherese.html"},
+ {"name": "Moy Family Association",
+  "address": "2238 S Wentworth Ave",
+  "position": {"lat": 41.851749300, "lng": -87.632259500},
+  "icon": "",
+  "link": ""},
+ {"name": "ChinaTown Public Library",
+  "address": "2100 S Wentworth Ave",
+  "position": {"lat": 41.853860, "lng": -87.632160},
+  "icon": "/images/map-icons/books.png",
+  "link": "./Location/Library.html"},
+ {"name": "Chinatown Square Plaza",
+  "address": "2133 S China Pl",
+  "position": {"lat": 41.853490, "lng": -87.635407},
+  "icon": "",
+  "link": ""},
+ {"name": "Chinese American Service League / CBCAC",
+  "address": "2141 S Tan Ct",
+  "position": {"lat": 41.854406300, "lng": -87.635565500},
+  "icon": "",
+  "link": ""},
+ {"name": "Ping Tom Park",
+  "address": "1700 S Wentworth Ave",
+  "position": {"lat": 41.859115900, "lng": -87.632596900},
+  "icon": "/images/map-icons/pinghut.png",
+  "link": "./Location/PingTomPark.html"}
+*/
+];
 
-var sttherese = {name: "St Therese Church",
-				 position: {lat: 41.851545200, lng: -87.632607200},
-				 icon: "/images/map-icons/StThereseChurchIcon.png",
-				 link: "./Location/ChinatownChurch-SaintTherese.html"};
-
-var ninedragon = {name: "Nine Dragon Wall",
-				  position: {lat: 41.853170000, lng: -87.631345000},
-				  icon: "/images/map-icons/NineDragonWallIcon.png",
-				  link: "./Location/NineDragonWall.html"};
-
-var gate = {position: {lat: 41.852697400, lng: -87.632211300},
-			icon: "",
-			link: ""};
-
-var puitak = {name: "Pui Tak Center",
-			  position: {lat: 41.852396900, lng: -87.632291100},
-			  icon: "/images/map-icons/PuiTakCenterIcon.png",
-			  link: "./Location/PuiTakCenter.html"};
-
-var buddhistassoc = {position: {lat: 41.851436200, lng: -87.631652300},
-					 icon: "",
-					 link: ""};
-
-var ccuc = {name: "Chinese Christian Union Church",
-			position: {lat: 41.850719000, lng: -87.631671600},
-			icon: "/images/map-icons/ChineseChristianUnionChurchIcon.png",
-			link: "./Location/ChineseChristianCatholicChurch.html"};
-
-var herbalist = {position: {lat: 41.849052000, lng: -87.632070700},
-				 icon: "",
-				 link: ""};
-
-var chineseshop = {position: {lat: 41.850397200, lng: -87.632185900},
-				   icon: "",
-				   link: ""};
-
-var ccamuseum = {name: "Chinese-American Museum",
-				 position: {lat: 41.851217700, lng: -87.633518200},
-				 icon: "/images/map-icons/dragon.png",
-				 link: "./Location/Chinese-AmericanMuseum.html"};
-
-var library = {name: "ChinaTown Public Library",
-			   position: {lat: 41.853860, lng: -87.632160},
-			   icon: "/images/map-icons/books.png",
-			   link: "./Location/Library.html"};
-
-var locations = [pingtom, sttherese, ninedragon, ccuc, ccamuseum, puitak, library];
 var location_elems = []; // Icons and maps added for locations
 
 var origin_lat = 41.848;
 var origin_long = -87.6400;
-// var origin_lat = 41.8477;
-// var origin_long = -87.6419
 
 var origin_lat_end = 41.8595;
 var origin_long_end = -87.6300;
-// var origin_lat_end = 41.8576;
-// var origin_long_end = -87.6302;
 
 var map_width_coords = origin_long_end - origin_long;
 var map_height_coords = origin_lat_end - origin_lat;
@@ -77,12 +98,7 @@ main_map.setAttribute("id", "main_map");
 main_map.setAttribute("alt", "map");
 main_map.setAttribute("width", map_width.toString());
 main_map.setAttribute("height", map_height.toString());
-// main_map.setAttribute("usemap", "#ChinaTown Clickable Map");
 document.getElementById("map").appendChild(main_map);
-
-// var clickable = document.createElement("map");
-// clickable.setAttribute("name", "ChinaTown Clickable Map");
-// document.getElementById("map").appendChild(clickable);
 
 function mapOpen(v) {
 	var icon_width = 32;
