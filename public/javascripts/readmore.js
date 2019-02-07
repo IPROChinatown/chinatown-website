28f6d06b929297b68ca89aeceb217ad081e5d202
SomethingCWC <17huangj@badinhs.org>
2019-02-07 17:44:53 -0600

    added scripts and head. edited location page

---
+++ public/javascripts/readmore.js
@@ -0,0 +1,15 @@
+﻿function readMoreFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("moreBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
---
+++ public/javascripts/readmoreZh.js
@@ -0,0 +1,15 @@
+﻿function readMoreFunctionZh() {
+    var dots = document.getElementById("dot");
+    var moreText = document.getElementById("moreZh");
+    var btnText = document.getElementById("moreBtnZh");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "更多";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "收起";
+        moreText.style.display = "inline";
+    }
+}
